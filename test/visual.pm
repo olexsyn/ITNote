6c02767e8ce630ea057f6deeffccfca69c0351c1
Olex Syn <olexsyn@users.noreply.github.com>
2021-01-15 21:21:19 +0200

    Create visual.pm

---
+++ test/visual.pm
@@ -0,0 +1,10 @@
+## Visual
+
+<span class="ques">?</span> question
+
+<span class="info">i</span> info
+
+<span class="warn">!</span> danger
+
+
+<span class="ques">test</span> <span class="ques">teg1</span> <span class="ques">tegged2</span>
